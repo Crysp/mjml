b99df40512d9a9c2789ea6a2ffeaee6027c33130
GitHub Action <action@github.com>
2021-02-19 15:11:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/crysp/mjml/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/crysp/mjml/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.crysp.mjml.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.crysp.mjml.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/crysp/mjml/services/MyProjectService.kt
+++ src/main/kotlin/com/github/crysp/mjml/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.crysp.mjml.services
 
+import com.github.crysp.mjml.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
